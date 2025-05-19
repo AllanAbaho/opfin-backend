cd70fa672cdf7de2ccd94b7667b7765bbeef6bda
Allan Abaho <abahoallans@gmail.com>
2025-05-19 10:31:33 +0300

    login page

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,22 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+// Redirect root to login
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+Auth::routes(['register' => false]);
 Route::get('/privacy-policy', function () {
     return view('privacy-policy');
 })->name('privacy.policy');
 // Account deletion routes
 Route::get('/account/delete', [AuthController::class, 'showDeleteForm'])->name('account.delete');
 Route::delete('/account/delete', [AuthController::class, 'destroy'])->name('account.destroy');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
